5d776e8c73e60a53ba9fbc87c0b0728cf7f80e77
Russell Best <ellrussbest@gmail.com>
2022-05-30 20:21:45 +0300

    changes in .sh files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-cd ../"`dirname $0`"
-source scripts/flags.sh
-cargo build --all --target wasm32-unknown-unknown --release
+
+RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
